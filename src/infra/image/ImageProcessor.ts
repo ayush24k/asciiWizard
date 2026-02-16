2d022ac2c2edcc2f467597fc3dcf1825580ea59c
ayush24k <ayushanshu69@gmail.com>
2026-02-16 20:09:46 +0530

    added imageProcessor

---
+++ src/infra/image/ImageProcessor.ts
@@ -0,0 +1,33 @@
+import sharp from "sharp";
+
+export interface ProcessedImage {
+    data: Uint8Array;
+    width: number;
+    height: number;
+}
+
+export class ImageProcessor {
+    async loadGrayscale(
+        path: string,
+        width: number
+    ): Promise<ProcessedImage> {
+        const image = sharp(path).resize({ width }).grayscale();
+
+        const { data, info } = await image
+            .raw()
+            .toBuffer({ resolveWithObject: true });
+
+        // data is RGB even in grayscale → take one channel
+        const grayscale = new Uint8Array(info.width * info.height);
+
+        for (let i = 0; i < grayscale.length; i++) {
+            grayscale[i] = data[i * info.channels]!;
+        }
+
+        return {
+            data: grayscale,
+            width: info.width,
+            height: info.height
+        };
+    }
+}
